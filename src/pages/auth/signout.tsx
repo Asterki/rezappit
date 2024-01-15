4dff0a832835f48939a44b340c976578f40110bb
Fernando Rivera <asterki.dev@proton.me>
2024-01-15 13:33:57 -0600

    Fix: Exposed login client

--- src/pages/auth/signout.tsx
+++ src/pages/auth/signout.tsx
@@ -1,81 +1,67 @@
 import { useRouter } from "next/router";
 import { useSession, signOut } from "next-auth/react";
-import { useTranslation } from "react-i18next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-import Navbar from "@/components/navbar";
 import Head from "next/head";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSignOutAlt, faBan } from "@fortawesome/free-solid-svg-icons";
 
-import { GetStaticProps, InferGetStaticPropsType } from "next";
 
-interface Props {}
+const SignOut = () => {
+	const router = useRouter();
+	const { data: session, status: loggedInStatus } = useSession({
+		required: true,
+		onUnauthenticated() {
+			router.push(`/${router.locale}/auth/signin`);
+		},
+	});
 
-const SignOut = (_props: InferGetStaticPropsType<typeof getStaticProps>) => {
-    const router = useRouter();
-    const { t } = useTranslation(["auth/signout", "components/navbar"]);
-    const { data: session, status: loggedInStatus } = useSession({
-        required: true,
-        onUnauthenticated() {
-            router.push(`/${router.locale}/auth/signin`);
-        },
-    });
+	return (
+		<div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white">
+			<Head>
+				<title></title>
+			</Head>
 
-    return (
-        <div className="text-white bg-dark1">
-            <Navbar t={t} session={session} />
+			{loggedInStatus == "loading" && (
+				<main className="absolute w-full min-h-screen text-white bg-dark1">
+					<p className="text-2xl text-center text-primary font-bold transition-all duration-500 transform hover:scale-105">
+						Loading...
+					</p>
+				</main>
+			)}
 
-            <Head>
-                <title>{t("pageTitle")}</title>
-            </Head>
+			{loggedInStatus == "authenticated" && (
+				<main className="min-h-screen flex flex-col justify-center items-center lg:p-12">
+					<section className="md:w-4/12 w-11/12">
+						<div className="my-4 text-gray-100 text-center">
+							<h1 className="text-3xl"></h1>
+							<p></p>
+						</div>
 
-            {loggedInStatus == "loading" && (
-                <main className="absolute w-full min-h-screen text-white bg-dark1">
-                    <p className="text-2xl text-center text-primary font-bold transition-all duration-500 transform hover:scale-105">
-                        Loading...
-                    </p>
-                </main>
-            )}
+						<button
+							className="bg-white/20 hover:bg-red1 w-full shadow-md rounded-md p-4 transition-all"
+							onClick={() => signOut({ redirect: true, callbackUrl: "/" })}
+						>
+							<FontAwesomeIcon icon={faSignOutAlt} className="mr-2" />
+							Sign Out
+						</button>
 
-            {loggedInStatus == "authenticated" && (
-                <main className="min-h-screen flex flex-col justify-center items-center lg:p-12">
-                    <section className="md:w-4/12 w-11/12">
-                        <div className="my-4 text-gray-100 text-center">
-                            <h1 className="text-3xl">{t("title")}</h1>
-                            <p>{t("desc")}</p>
-                        </div>
+						<br />
+						<br />
 
-                        <button
-                            className="bg-white/20 hover:bg-red1 w-full shadow-md rounded-md p-4 transition-all"
-                            onClick={() => signOut({ redirect: true, callbackUrl: "/" })}
-                        >
-                            <FontAwesomeIcon icon={faSignOutAlt} className="mr-2" />
-                            {t("buttons.signout")}
-                        </button>
-
-                        <br />
-                        <br />
-
-                        <button
-                            className="bg-white/20 hover:bg-primary w-full shadow-md rounded-md p-4 transition-all"
-                            onClick={() => router.push(`/${router.locale}/app`)}
-                        >
-                            <FontAwesomeIcon icon={faBan} className="mr-2" />
-                            {t("buttons.cancel")}
-                        </button>
-                    </section>
-                </main>
-            )}
-        </div>
-    );
+						<button
+							className="bg-white/20 hover:bg-primary w-full shadow-md rounded-md p-4 transition-all"
+							onClick={() => router.push(`/${router.locale}/app`)}
+						>
+							<FontAwesomeIcon icon={faBan} className="mr-2" />
+							Cancel
+						</button>
+					</section>
+				</main>
+			)}
+		</div>
+	);
 };
 
-export const getStaticProps: GetStaticProps<Props> = async ({ locale }) => ({
-    props: {
-        ...(await serverSideTranslations(locale ?? "en", ["auth/signout", "components/navbar"])),
-    },
-});
 
 export default SignOut;
