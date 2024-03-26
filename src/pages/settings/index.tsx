7018348e2ca0310bef129d26abf9014aae06cba4
asterki <asterki.dev@proton.me>
2024-03-26 15:08:56 -0600

    Fix: General setting page renewed

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,52 +1,67 @@
 import * as React from "react";
+import axios, { AxiosResponse } from "axios";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import { useTranslation } from "next-i18next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
+import Image from "next/image";
 import NavbarComponent from "@/components/navbar";
 import SettingsLeftBarComponent from "@/components/profile/settingsLeftBar";
+import NotificationComponent from "@/components/notification";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+import { useTranslation } from "next-i18next";
 
 import type { GetStaticProps, InferGetStaticPropsType } from "next";
 type Props = {};
 
-const SettingsIndexPage = (_props: InferGetStaticPropsType<typeof getStaticProps>) => {
+const SettingsGeneral = (_props: InferGetStaticPropsType<typeof getStaticProps>) => {
 	const router = useRouter();
 	const { t } = useTranslation(["main/index", "components/navbar"]);
-	const { data: session, status: loggedInStatus } = useSession({
+	const {
+		data: session,
+		status: loggedInStatus,
+		update,
+	} = useSession({
 		required: true,
 	});
+
+	// Modal related
+	const [showingModal, setShowingModal] = React.useState<boolean>(false);
+	const [modalInfo, setModalInfo] = React.useState<{
+		title: string;
+		message: string;
+		type: "success" | "error" | "info";
+		dismissButtonText: string;
+	}>({
+		title: "",
+		message: "",
+		type: "success",
+		dismissButtonText: "",
+	});
+
 	return (
-		<div className="text-white bg-dark-1 min-h-screen flex justify-center">
-			<NavbarComponent />
+		<div className="text-white bg-dark-1 min-h-screen flex justify-center w-full">
+			<NavbarComponent user={session?.user} />
 
-			{loggedInStatus == "authenticated" ? (
-				<main className="flex justify-around mt-20 w-10/12 ">
-					<SettingsLeftBarComponent router={router} activeCategory="general" />
+			<NotificationComponent
+				showing={showingModal}
+				setShowing={setShowingModal}
+				type={modalInfo.type}
+				title={modalInfo.title}
+				message={modalInfo.message}
+				dismissButtonText={modalInfo.dismissButtonText}
+			/>
 
-					<div className="flex flex-col w-6/12 items-center my-12 rounded-md bg-neutral-900 p-4">
-						<h1 className="text-2xl font-bold text-center">Settings</h1>
+			{loggedInStatus == "authenticated" && session.user ? (
+				<main className="flex lg:flex-row flex-col mt-20 w-full">
+					<SettingsLeftBarComponent activeCategory="general" router={router} />
 
-						<section className="py-2">
-							<button className="font-semibold w-11/12 p-[10px] px-20 rounded-md bg-blue-400 border-2 border-blue-400 my-2 hover:brightness-110 transition-all">
-								Primary
-							</button>
-							<button className="font-semibold w-11/12 p-[10px] px-20 rounded-md bg-neutral-800 border-2 border-neutral-600 my-2 hover:brightness-110 transition-all">
-								Sec
-							</button>
-							<button className="font-semibold w-11/12 p-[10px]  px-20 rounded-md bg-gray-300 border-2 border-gray-400 my-2 hover:brightness-110 transition-all">
-								Third
-							</button>
-							<button className="font-semibold w-11/12 p-[10px] px-20 rounded-md bg-red-500 border-2 border-red-600 my-2 hover:brightness-110 transition-all">
-								Destructive
-							</button>
-						</section>
+					<div className="flex flex-col lg:w-6/12 w-full my-12 rounded-md p-4">
+						<h1 className="text-3xl font-bold">General Settings</h1>
 					</div>
 				</main>
 			) : (
-				<main className="flex justify-around mt-20 w-10/12">
-                    Loading your session...
-                </main>
+				<main className="flex justify-around mt-20 w-10/12">Loading your session...</main>
 			)}
 		</div>
 	);
@@ -58,4 +73,4 @@ export const getStaticProps: GetStaticProps<Props> = async ({ locale }) => ({
 	},
 });
 
-export default SettingsIndexPage;
+export default SettingsGeneral;
