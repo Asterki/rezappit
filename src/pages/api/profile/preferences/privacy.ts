82386ce386500a5a97dcf27edf1119ce82ecdd38
asterki <asterki.dev@proton.me>
2024-03-26 15:17:31 -0600

    Feat: Added method to fetch privacy-related preferences

--- src/pages/api/profile/preferences/privacy.ts
+++ src/pages/api/profile/preferences/privacy.ts
@@ -18,33 +18,43 @@ interface RequestBody {
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-	if (req.method !== "POST") return res.status(405).json({ message: "method-not-allowed" });
-
 	const session = await getServerSession(req, res, authOptions);
-    if (!session) return res.status(401).json({ message: "unauthorized" });
+	if (!session) return res.status(401).json({ message: "unauthorized" });
 
-	try {
-		const parsedBody = z
-			.object({
-                hideEmail: z.enum(["everyone", "friends", "none"]),
-                hideProfile: z.enum(["everyone", "friends", "none"]),
-                hideActivity: z.enum(["everyone", "friends", "none"]),
-                hideProfilePicture: z.enum(["everyone", "friends", "none"]),
-			})
-			.safeParse(req.body);
+	if (req.method == "GET") {
+		try {
+			const data = await userdata.findById(session.user!.id);
+			if (!data) return res.status(404).json({ message: "not-found" });
 
-		if (parsedBody.success === false) return res.status(400).json({ message: "bad-request" });
+			return res.status(200).json(data.preferences.privacy);
+		} catch (error) {
+			return res.status(500).json({ message: "server-error" });
+		}
+	} else if (req.method == "POST") {
+		try {
+			const parsedBody = z
+				.object({
+					hideEmail: z.enum(["everyone", "friends", "none"]),
+					hideProfile: z.enum(["everyone", "friends", "none"]),
+					hideActivity: z.enum(["everyone", "friends", "none"]),
+					hideProfilePicture: z.enum(["everyone", "friends", "none"]),
+				})
+				.safeParse(req.body);
 
-        const data = await userdata.findById(session.user!.id);
-        if (!data) return res.status(404).json({ message: "not-found" });
+			if (parsedBody.success === false) return res.status(400).json({ message: "bad-request" });
 
-        data.preferences.privacy = parsedBody.data;
-        await data.save();
+			const data = await userdata.findById(session.user!.id);
+			if (!data) return res.status(404).json({ message: "not-found" });
 
+			data.preferences.privacy = parsedBody.data;
+			await data.save();
 
-        return res.status(200).json({ message: "success" });
-	} catch (error) {
-		return res.status(500).json({ message: "server-error" });
+			return res.status(200).json({ message: "success" });
+		} catch (error) {
+			return res.status(500).json({ message: "server-error" });
+		}
+	} else {
+		return res.status(405).json({ message: "method-not-allowed" });
 	}
 }
 
